c3aa1c911987cff7a5100d99ae7a1de6d7ee99e9
Andrea Rescalli <rescalli.are@gmail.com>
2020-10-24 23:56:13 +0200

    Modulation of LED through potentiometer implemented

--- RESCALLI_ANDREA.cydsn/main.c
+++ RESCALLI_ANDREA.cydsn/main.c
@@ -15,6 +15,10 @@
 
 #define ON 1
 
+//int32 value_digit = 0;
+//int32 value_mv = 0;
+float dc[2] = {0};
+
 
 int main(void)
 {
@@ -25,27 +29,35 @@ int main(void)
     PWM_LED_Start();
     
     flag_timer = 0;
-    value_digit = 0;
-    value_mv = 0;
-    int32 dc[2] = {0};
     
     isr_Timer_StartEx(Custom_ISR_Timer);
 
     //Pin_LED_Write(ON);
     ADC_DelSig_StartConvert();
     
-    PWM_LED_WriteCompare(0);
+    //PWM_LED_WriteCompare(0);
     
 
     for(;;)
     {
         if(flag_timer) {
+            
+//            value_digit = ADC_DelSig_Read32();
+//            if(value_digit < 0) {
+//                value_digit = 0;
+//            }
+//    
+//            if(value_digit > 65535) {
+//                value_digit = 65535;
+//            }
+//            
+            
             dc[0] = dc[1];
             UART_PutString(DataBuffer);
             flag_timer = 0;
-            dc[1] = value_digit/65535*100;
-            if((dc[1] >= dc[0]-1/100*dc[0]) || (dc[1] <= dc[0]+1/100*dc[0])) {
-                //PWM_LED_WriteCompare(dc[1]);
+            dc[1] = value_digit/65535.0f*100;
+            if((dc[1] >= dc[0]-1/100.0f*dc[0]) || (dc[1] <= dc[0]+1/100.0f*dc[0])) {
+                PWM_LED_WriteCompare((uint8)dc[1]);
             }
             
         }
